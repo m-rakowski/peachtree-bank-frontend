1b4d773b490e0be3070acee6f914957a4fe8e2a6
m-rakowski <michalrakowski@yahoo.com>
2021-11-23 21:37:32 +0000

    account.service.spec.ts, as-form-control.pipe.spec.ts

--- src/app/modules/core/pipes/as-form-control.pipe.spec.ts
+++ src/app/modules/core/pipes/as-form-control.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { AsFormControlPipe } from './as-form-control.pipe';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 
 describe('AsFormControlPipe', () => {
-  it('create an instance', () => {
+  it('should have return type of FormControl', () => {
     const pipe = new AsFormControlPipe();
-    expect(pipe).toBeTruthy();
+
+    const formGroup = new FormGroup({
+      control: new FormControl(),
+    });
+
+    expect(
+      pipe.transform(formGroup.get('control') as AbstractControl)
+    ).toBeInstanceOf(FormControl);
   });
 });
